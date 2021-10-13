c58366b13a26ba5ab6532d1726b7b145b7c73ba8
Cassandra Barragan <50923891+cassbarragan@users.noreply.github.com>
2021-10-12 19:43:00 -0700

    styling first pass. All elements rendering properly.

--- client/src/components/entryList.jsx
+++ client/src/components/entryList.jsx
@@ -3,12 +3,12 @@ import React, { useState, useContext } from "react";
 import Stack from "@mui/material/Stack";
 import { styled } from "@mui/material/styles";
 import Typography from "@mui/material/Typography";
-import { Button, Paper } from "@material-ui/core";
+import { Button, Paper, Box } from "@material-ui/core";
 import { dataContext } from "../context/dataContext.js";
 
 const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
-  padding: theme.spacing(1),
+  padding: theme.spacing(3),
   textAlign: "center",
   color: theme.palette.text.secondary,
 }));
@@ -21,11 +21,27 @@ const EntryList = () => {
       <Stack spacing={10}>
         {entries.map((item) => {
           return (
-            <Item onClick={() => console.log("clicked")}>
-              <Typography variant="h6" gutterBottom component="div">
-                {`${item.name} - ${item.date} - ${item.entry}`}
-              </Typography>
-            </Item>
+            <Box
+              component="span"
+              sx={{
+                display: "flex",
+                flexWrap: "wrap",
+                "& > :not(style)": {
+                  m: 1,
+                  width: "50rem"
+                },
+              }}
+            >
+              <Item onClick={() => console.log("clicked")}>
+                <Typography variant="h6" gutterBottom component="div">
+                  {item.date}
+                </Typography>
+                <Typography variant="h5" gutterBottom component="div">
+                  {item.name}
+                </Typography>
+                <Typography variant="body1">{item.entry}</Typography>
+              </Item>
+            </Box>
           );
         })}
       </Stack>
--- client/src/components/form.jsx
+++ client/src/components/form.jsx
@@ -1,52 +1,97 @@
 import React, { useState, useContext } from "react";
-import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
-import { Button, Paper } from "@material-ui/core";
-import {dataContext} from '../context/dataContext.js';
+import { Button, Paper, Box } from "@material-ui/core";
+import { dataContext } from "../context/dataContext.js";
+import axios from "axios";
+import styled from "styled-components";
 
+const FormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 3rem;
+  margin-bottom: 2rem;
+  align-items: center;
+  justify-content: center;
+`;
 
 const Form = () => {
-
   var { entries, setEntries } = useContext(dataContext);
-  const [ textValue, setTextValue ] = useState({});
+  const [nameValue, setNameValue] = useState("");
+  const [dateValue, setDateValue] = useState("");
+  const [textValue, setTextValue] = useState("");
 
-  console.log('text value:', textValue)
+  console.log(
+    "name value:",
+    nameValue,
+    "datevalue:",
+    dateValue,
+    "text value:",
+    textValue
+  );
 
   const addEntry = async (entry) => {
     const entryName = entry.name;
     const entryDate = entry.date;
     const entryText = entry.text;
     await axios
-    .post(`/addEntry`, {
-      name: entryName,
-      date: entryDate,
-      entry: entryText
-    })
-    .then(() => res.status(200).send('submitted'))
-    .catch(() => res.status(404).send('error submitting'))
-  }
+      .post(`/addEntry`, {
+        name: entryName,
+        date: entryDate,
+        entry: entryText,
+      })
+      .then(() => res.status(200).send("submitted"))
+      .catch(() => res.status(404).send("error submitting"));
+  };
 
+  const onNameChange = (e) => setNameValue(e.target.value);
+  const onDateChange = (e) => setDateValue(e.target.value);
   const onTextChange = (e) => setTextValue(e.target.value);
 
-  const handleSubmit = () => addEntry(textValue);
+  const handleSubmit = () => addEntry({ nameValue, dateValue, textValue });
 
-  const handleReset = () => setTextValue("");
+  const handleReset = () => setNameValue("");
+  // setDateValue("")
+  // setTextValue("")
 
   return (
-    <form>
-     <TextField onChange={onTextChange} value={textValue} label={"Name"} />
-     <TextField onChange={onTextChange} value={textValue} label={"Date"} />
+    <FormContainer>
       <TextField
+        onChange={onNameChange}
+        value={nameValue}
+        label={"Name"}
+        sx={{ m: 2, width: "35ch" }}
+        id="filled-basic"
+        variant="filled"
+      />
+      <TextField
+        onChange={onDateChange}
+        value={dateValue}
+        label={"Date"}
+        sx={{ m: 2, width: "35ch" }}
+        id="filled-basic"
+        variant="filled"
+      />
+      <TextField
+        id="outlined-multiline-static"
+        multiline
+        rows={10}
         onChange={onTextChange}
         value={textValue}
         label={"Journal Entry"}
+        sx={{ m: 2, width: "70ch" }}
+        id="filled-basic"
+        variant="filled"
       />
-     <Button onClick={handleSubmit}>Submit</Button>
-     <Button onClick={handleReset}>Reset</Button>
-    </form>
-
-
+      <Box sx={{ '& button': { m: 1 } }} >
+      <Button variant="contained" size="small" onClick={handleSubmit}>
+        Submit
+      </Button>
+      <Button variant="contained" size="small" onClick={handleReset}>
+        Reset
+      </Button>
+      </Box>
+    </FormContainer>
   );
 };
 
